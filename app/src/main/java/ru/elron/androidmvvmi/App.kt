e1bb4e129261e1bf04c1d68b111f2706842b53b1
elron <p_a_u_l2003@rambler.ru>
2022-03-04 02:02:07 +0300

    update todo

--- app/src/main/java/ru/elron/androidmvvmi/App.kt
+++ app/src/main/java/ru/elron/androidmvvmi/App.kt
@@ -5,10 +5,6 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import ru.elron.androidmvvmi.di.appModule
 
-/**
- * приложение список дел
- * стек: DataBinding, ViewModel, Coroutines, LiveData, 	Room, Retrofit, Koin,	GSON
- */
 class App : Application() {
     companion object {
         lateinit var INSTANCE: App
